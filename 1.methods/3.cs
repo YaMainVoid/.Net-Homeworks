2e1ec4f94f8eeb32accc54241310e416cebbf0c1
YaMainVoid <43910194+YaMainVoid@users.noreply.github.com>
2019-03-11 21:24:35 +0300

    Add files via upload

---
+++ 1.methods/3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool CompareArrays(int[] a, int[] b)
+        {
+            if (a == null || b == null) { return false; }
+            if (a.Length == 0 && b.Length == 0) { return true; }
+            if (a.Length != b.Length) { return false; }
+            Array.Sort(a);
+            Array.Sort(b);
+            for (int i = 0; i < a.Length; i++)
+            {
+                int square = a[i] * a[i];
+                if (square != b[i]) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
+            Console.WriteLine(CompareArrays(a, b));
+        }
+    }
+}
